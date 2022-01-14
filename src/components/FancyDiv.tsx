8fb2f21675da238051e1a4083dd2e595fc8b0118
Sam Ludwig <sam.ludwig@engagesq.com>
2022-01-14 20:10:34 +1100

    x

--- src/components/FancyDiv.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const FancyDiv: React.FC = ({ children }) => {
-  return <div style={{ fontSize:'24px' }}>{children}</div>
-}
-export default FancyDiv
--- src/pages/about.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../pageStyles.css'
-import CovidCount from "../components/CovidCount";
-export default () => (
-  <div style={{display:'flex', justifyContent:'center'}} className='page'>
-    <CovidCount/>
-  </div>
-)

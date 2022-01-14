438db21281b873285d8852054516b17842813a22
Sam Ludwig <sam.ludwig@engagesq.com>
2022-01-14 19:18:59 +1100

    Updates

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-// Allow importing images,
-// you can add other formats supported by file-loader
-declare module '*.jpg'
-declare module '*.png'
-declare module '*.module.scss'
-declare module '*.scss'
-// declare module '@animated-burgers/burger-slip'
